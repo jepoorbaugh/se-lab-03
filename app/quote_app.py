ac28c0d9c5232ef7c3df064160ba1ecbb4fe57ed
jepoorbaugh <jepoorbaugh@gmail.com>
2024-09-27 11:30:19 -0400

    Add page for searching topics

--- app/quote_app.py
+++ app/quote_app.py
@@ -12,11 +12,27 @@ quote_data = pd.read_csv("filtered_quotes.csv")
 
 @app.route("/")
 def quote_of_the_day():
-    # Get quote of the day 
+    # Get quote of the day
     quote_index = (datetime.datetime.now() - datetime.datetime(1970, 1, 1)).days % len(
         quote_data
     )
     quote_info = quote_data.iloc[quote_index].to_dict()
     return render_template(
         "index.html", quote=quote_info["quote"], author=quote_info["author"]
+    )
+
+
+@app.route("/topic/<topic>")
+def topic_page(topic):
+    # Get quotes with topic
+    category_quote_data = quote_data[
+        # Categories are stored as a comma-delimited string, so we need to parse it.
+        quote_data["category"].apply(lambda categories: topic in categories.split(", "))
+    ].sort_values("author")
+
+    return render_template(
+        "topic.html",
+        num_results=len(category_quote_data),
+        topic=topic,
+        quotes=category_quote_data.to_records(),
     )

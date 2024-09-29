0e2b03dbfc157e4b6152b980c6fd0149cb86f979
jepoorbaugh <jepoorbaugh@gmail.com>
2024-09-29 15:10:46 -0400

    Add forms to allow users to navigate between pages

--- app/quote_app.py
+++ app/quote_app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, url_for, request, redirect
 import pandas as pd
 from random import randint
 import datetime
@@ -8,18 +8,31 @@ app = Flask(__name__)
 
 
 quote_data = pd.read_csv("filtered_quotes.csv")
+categories = quote_data["category"].apply(lambda x: x.split(", ")).explode().unique()
+authors = quote_data["author"].unique()
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def quote_of_the_day():
-    # Get quote of the day
-    quote_index = (datetime.datetime.now() - datetime.datetime(1970, 1, 1)).days % len(
-        quote_data
-    )
-    quote_info = quote_data.iloc[quote_index].to_dict()
-    return render_template(
-        "index.html", quote=quote_info["quote"], author=quote_info["author"]
-    )
+    if request.method == "GET":
+        # Get quote of the day
+        quote_index = (
+            datetime.datetime.now() - datetime.datetime(1970, 1, 1)
+        ).days % len(quote_data)
+        quote_info = quote_data.iloc[quote_index].to_dict()
+        return render_template(
+            "index.html",
+            quote=quote_info["quote"],
+            author=quote_info["author"],
+            topic_options=categories,
+            author_options=authors,
+        )
+    elif request.method == "POST":
+        # Deal with redirecting users
+        if "topic" in request.form.keys():
+            return redirect(url_for("topic_page", topic=request.form["topic"]))
+        elif "author" in request.form.keys():
+            return redirect(url_for("author_page", author=request.form["author"]))
 
 
 @app.route("/topic/<topic>")
@@ -32,6 +45,8 @@ def topic_page(topic):
 
     return render_template(
         "quote_list.html",
+        topic_options=categories,
+        author_options=authors,
         num_results=len(category_quote_data),
         topic=topic,
         quotes=category_quote_data.to_records(),
@@ -45,6 +60,8 @@ def author_page(author):
 
     return render_template(
         "quote_list.html",
+        topic_options=categories,
+        author_options=authors,
         num_results=len(author_quote_data),
         topic=author,
         quotes=author_quote_data.to_records(),
